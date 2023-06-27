a33d3285239f0d084505c493ee541fd8cbe2779f
Augusto Collerone <collerone.augusto@gmail.com>
2023-06-27 16:22:10 -0300

    Whitelist working & Verifying almost

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,29 @@
 const CaptureTheFlag = artifacts.require("CaptureTheFlag");
 
 const WhiteListPaymaster = artifacts.require("WhitelistPaymaster");
+const RelayHub = artifacts.require("RelayHub");
+
+// const RelayHubContract = require("./RelayHub.json");
 
 module.exports = async function (deployer) {
   const forwarder = require("../build/gsn/Forwarder").address;
   await deployer.deploy(CaptureTheFlag, forwarder);
+
+  const relayHubAddress = require("../build/gsn/RelayHub.json").address;
+  const relayHub = await RelayHub.at(relayHubAddress);
+
+  // const relayHub = new web3.eth.Contract(RelayHubContract.abi, relayHubAddress);
 
   await deployer.deploy(WhiteListPaymaster);
   const pm = await WhiteListPaymaster.deployed();
 
   await pm.setTrustedForwarder(forwarder);
 
-  await pm.whitelistSender("0x1dF62f291b2E969fB0849d99D9Ce41e2F137006e");
+  await pm.whitelistSender("0x1dF62f291b2E969fB0849d99D9Ce41e2F137006e", true);
+  await pm.whitelistSender("0x58a78411bc31982E0D28527ceCE155dA1cd93c53", false);
+  await pm.setConfiguration(true, false, false, false);
 
-  const relayHubAddress = "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550";
+  await relayHub.depositFor(pm.address, { value: 1e18 });
 
   await pm.setRelayHub(relayHubAddress);
 
--- ui/index.js
+++ ui/index.js
@@ -5,9 +5,59 @@ const paymasterAddress = require("../build/gsn/Paymaster").address;
 const contractArtifact = require("../build/contracts/CaptureTheFlag.json");
 const contractAbi = contractArtifact.abi;
 
+const paymasterArtifact = require("../build/contracts/WhitelistPaymaster.json");
+const veryPaymasterArtifact = require("../build/contracts/VerifyingPaymaster.json");
+
 let theContract;
 let provider;
 let gsnProvider;
+
+const asyncApprovalData = async function (relayRequest) {
+  console.log("*AC im here in the async approval");
+  const signer = provider.getSigner();
+  console.log("*AC got the signer");
+  // Pack the ForwardRequest and RelayData just like in the smart contract
+  const packedForwardRequest = ethers.utils.defaultAbiCoder.encode(
+    ["address", "address", "uint256", "uint256", "uint256", "bytes"],
+    [
+      relayRequest.request.from,
+      relayRequest.request.to,
+      relayRequest.request.value,
+      relayRequest.request.gas,
+      relayRequest.request.nonce,
+      relayRequest.request.data,
+    ]
+  );
+
+  console.log("*AC STEP 1 ");
+  const packedRelayData = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256", "address", "address", "bytes", "string"],
+    [
+      relayRequest.relayData.maxFeePerGas,
+      relayRequest.relayData.maxPriorityFeePerGas,
+      relayRequest.relayData.relayWorker,
+      relayRequest.relayData.paymaster,
+      relayRequest.relayData.paymasterData,
+      relayRequest.relayData.clientId,
+    ]
+  );
+
+  // Hash the packed data
+  const requestHash = ethers.utils.keccak256(
+    ethers.utils.solidityPack(
+      ["bytes", "bytes"],
+      [packedForwardRequest, packedRelayData]
+    )
+  );
+
+  // Sign the hash
+  const signature = await signer.signMessage(
+    ethers.utils.arrayify(requestHash)
+  );
+
+  return signature;
+  //   return Promise.resolve("0x1234567890");
+};
 
 async function initContract() {
   if (!window.ethereum) {
@@ -21,20 +71,33 @@ async function initContract() {
     console.log("chainChained");
     window.location.reload();
   });
-  const networkId = await window.ethereum.request({ method: "net_version" });
 
-  gsnProvider = await RelayProvider.newProvider({
+  const networkId = await window.ethereum.request({ method: "net_version" });
+  const whitelistPaymasterAddress =
+    paymasterArtifact.networks[networkId].address;
+  const veryPaymasterAddress =
+    veryPaymasterArtifact.networks[networkId].address;
+
+  console.log("*AC whitelistPaymasterAddress: ", whitelistPaymasterAddress);
+
+  const { gsnProvider, gsnSigner } = await RelayProvider.newEthersV5Provider({
     provider: window.ethereum,
     config: {
       loggerConfiguration: { logLevel: "debug" },
-      paymasterAddress,
+      paymasterAddress: veryPaymasterAddress,
     },
-  }).init();
+    overrideDependencies: { asyncApprovalData },
+  });
 
-  provider = new ethers.providers.Web3Provider(gsnProvider);
+  console.log("*AC here1: ", gsnProvider);
+
+  provider = gsnProvider;
+  console.log("*AC here2");
 
   const network = await provider.getNetwork();
+  console.log("*AC here3");
   const artifactNetwork = contractArtifact.networks[networkId];
+  console.log("*AC here4");
   if (!artifactNetwork)
     throw new Error("Can't find deployment on network " + networkId);
   const contractAddress = artifactNetwork.address;
